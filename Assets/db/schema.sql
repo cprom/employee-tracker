c65d5ff436362da30a21a00504a078431121bfaf
cprom <chen.prom@gmail.com>
2020-03-01 20:56:32 -0800

    update DB

--- Assets/db/schema.sql
+++ Assets/db/schema.sql
@@ -5,24 +5,28 @@ CREATE DATABASE employeeTracker_db;
 USE employeeTracker_db;
 
 CREATE TABLE employee (
-  employee_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  first_name VARCHAR(30) NULL,
-  last_name VARCHAR(30) NULL,
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  manager_id INT 
-  FOREIGN KEY (role_id) REFERENCES role(role_id)
-  FOREIGN KEY (manager_id) REFERENCES employee(manager_id)
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
+
+
 
 CREATE TABLE role (
-  role_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  title VARCHAR(30) NULL,
-  salary DECIMAL NULL,
-  department_id INT NULL
-  FOREIGN KEY (department_id) REFERENCES department(department_id)
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
+
+
 CREATE TABLE department (
-  department_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  department_name VARCHAR(30)
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30) NOT NULL
 );
