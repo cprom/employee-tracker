dc61048398b680fb69320013f69d82ec2ce77044
cprom <chen.prom@gmail.com>
2020-03-02 02:21:37 -0800

    add prompts

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const prompt = require('inquirer').createPromptModule()
 const mysql = require('mysql')
 const consoletable = require('console.table')
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
@@ -20,19 +20,92 @@ employeeTracker = () => {
     choices: ['View All Employees', 'View All Department', 'View Roles', 'Add Employee', 'Add Role', 'Add Department', 'Update Employee Role']
   })
     .then((options) => {
-      console.log(options)
+      //console.table(options)
       switch (options) {
         case 'View All Employees':
           viewAllEmployees()
           break;
+        case 'View All Department':
+          viewDepartment()
+          break;
+        case 'View Roles':
+          viewRoles()
+          break;
+        case 'Add Employee':
+          addEmployee()
+          break;
+        case 'Add Role':
+          addRole()
+          break;
+        case 'Add Department':
+          addDepartment()
+          break;
+        case 'Update Employee Role':
+          updateRole()
+          break;
       }
     })
     .catch(e => console.log(e))
+
+
 }
 
 viewAllEmployees = () => {
-  console.log('ping')
+  const query = 'SELECT * FROM employee';
+  connection.query(query, (err, res) => {
+    if (err) return err
+    console.table(res)
+    employeeTracker()
+  })
 }
+
+viewDepartment = () => {
+  const query = 'SELECT * FROM department'
+  connection.query(query, (err, res) => {
+    console.table(res)
+    employeeTracker()
+  })
+}
+
+viewRoles = () => {
+  const query = 'SELECT * FROM role'
+  connection.query(query, (err, res) => {
+    console.table(res)
+    employeeTracker()
+  })
+}
+
+addEmployee = () => {
+  prompt(
+    {
+      type: 'input',
+      name: 'first_name',
+      message: 'First name:'
+    },
+    {
+      type: 'input',
+      name: 'last_name',
+      message: 'Last name:'
+    },
+    {
+      type: 'list',
+      name: 'department',
+      message: 'Department:',
+      choices: ['Engineering', 'HR', 'IT', 'Accounting', 'Sales']
+    },
+    {
+      type: 'input',
+      name: 'role',
+      message: "Enter employee's role:"
+    },
+    {
+      type: 'input',
+      name: 'manager',
+      message: "Enter employee's manager:"
+    }
+  )
+}
+
 
 employeeTracker()
 
